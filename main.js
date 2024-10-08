9aba456b59d3acce0cbdff7b5fa2bf7843f6f4df
Cayla Wanzer <caylawanzer6@gmail.com>
2024-10-08 13:54:16 -0400

    math

--- main.js
+++ main.js
@@ -10,8 +10,33 @@ f = 99.999 // fraction, float ( floating poiont decimal)
 
 num = 1.1 // just numbers
 
-text = "123.45 words" // string of characters
+text = "123 words" // string of characters - data type string
 number = parseFloat(text)
 double = number * 2 // now we can do math 
 console.log(double)
 // F5 - run/execute/debug
+
+d = new Date()
+console.log(d)
+console.log(d.getTime())
+
+let counter = 0
+counter++ // increment - bump up by one
+counter++
+console.log(counter)
+counter-- // decrement - decrease by one 
+console.log(counter) 
+
+let level = 0 
+level += 5 // bump up by 5
+level += 5
+level += 5
+console.log(level)
+level -= 5 // bump down by 5
+console.log(level)
+
+console.log(3+5) // + ( plus) is addition 
+console.log(3-5) // (minus) subtraction
+console.log(3*5) // (times) multiplication 
+console.log(3/5) // (over) division 
+console.log()
