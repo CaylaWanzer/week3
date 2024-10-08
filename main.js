ef20cf22b3ac0584623adf31959faa6c68c4e6ad
Cayla Wanzer <caylawanzer6@gmail.com>
2024-10-08 13:30:12 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("TUESDAY");
+// single line comment 
+/* wraps 
+a multi line comment */
+n = 12 // super chill
+let x = 123; // more formal
+
+i = 123 // integer (whole number)
+f = 99.999 // fraction, float ( floating poiont decimal)
+
+num = 1.1 // just numbers
+
+text = "123.45 words" // string of characters
+number = parseFloat(text)
+double = number * 2 // now we can do math 
+console.log(double)
+// F5 - run/execute/debug
